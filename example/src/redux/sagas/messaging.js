543930b3d3a9b919c970f1bdd4a9fbb5c5426bad
Nathan Gaberel <nathang@theodo.co.uk>
2017-09-17 00:15:01 +0100

    Fix messaging in example app.

--- example/src/redux/sagas/messaging.js
+++ example/src/redux/sagas/messaging.js
@@ -5,21 +5,39 @@ import { setRegistrationToken } from '../reducer/messaging.actions';
 
 import rsf from '../rsf';
 
-export default function*() {
+function* requestPermissionSaga() {
   const messaging = firebase.messaging();
 
-  const permisson = yield messaging.requestPermission();
-  const token = yield messaging.getToken();
+  try {
+    yield messaging.requestPermission();
+    const token = yield messaging.getToken();
+    yield put(setRegistrationToken(token));
+  } catch(error) {
+    console.warn('Notifications blocked')
+  }
+}
 
-  yield put(setRegistrationToken(token));
-
+function* messageHandlerSaga() {
   const messageChannel = rsf.messaging.channel();
+
   yield takeEvery(messageChannel, function*(message) {
     console.log('You\'ve got mail!', message);
   });
+}
 
+function* tokenRefreshSaga() {
   const tokenRefreshChannel = rsf.messaging.tokenRefreshChannel();
+
   yield takeEvery(tokenRefreshChannel, function*(token) {
     yield put(setRegistrationToken(token));
   });
+}
+
+export default function*() {
+  yield requestPermissionSaga()
+
+  yield [
+    messageHandlerSaga,
+    tokenRefreshSaga
+  ]
 }
