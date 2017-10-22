7a7dac1b14ae74005c7232b661dc643f77f36a0f
Nathan Gaberel <nathang@theodo.co.uk>
2017-10-22 01:05:11 +0100

    Setup firestore in example app.

--- example/src/redux/rsf.js
+++ example/src/redux/rsf.js
@@ -1,14 +1,19 @@
 import firebase from 'firebase';
+import '@firebase/firestore';
 import ReduxSagaFirebase from '../../../src/index';
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyCSTkbHZIcJluamfb69ShSHXn8351H9Vm0",
   authDomain: "redux-saga-firebase.firebaseapp.com",
   databaseURL: "https://redux-saga-firebase.firebaseio.com",
+  projectId: "redux-saga-firebase",
   storageBucket: "redux-saga-firebase.appspot.com",
-  messagingSenderId: "231632772443",
+  messagingSenderId: "231632772443"
 });
 
-const rsf = new ReduxSagaFirebase(firebaseApp);
+const rsf = new ReduxSagaFirebase(
+  firebaseApp,
+  firebase.firestore()
+);
 
 export default rsf;
