805509f97ad5264e3975778ce99c03cf21ee0ea7
BrunoGodefroy <brunog@theodo.co.uk>
2017-11-06 09:29:39 +0100

    Give a failureActionCreator to be dispatched on channel failure

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,14 @@ import { cancelled, put, take } from 'redux-saga/effects'
 
 export const noop = x => x
 
-export function * syncChannel (channel, successActionCreator, transform = noop) {
+export function * syncChannel (channel, successActionCreator, transform = noop, failureActionCreator = null) {
   try {
     while (true) {
       const data = yield take(channel)
       yield put(successActionCreator(transform(data)))
     }
+  } catch (err) {
+    if (failureActionCreator) yield put(failureActionCreator(err))
   } finally {
     if (yield cancelled()) channel.close()
   }
