bcf03082700b530ee29463e88ad95b7fd795952a
Nathan Gaberel <nathang@theodo.co.uk>
2017-12-28 15:29:03 +0100

    Styled components for molecules.

--- example/src/components/molecules/Example.js
+++ example/src/components/molecules/Example.js
@@ -1,9 +1,40 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import SyntaxHighlighter from 'react-syntax-highlighter'
+import styled from 'styled-components'
 import hljsStyle from './hljs-style'
 
-import './Example.styl'
+const StyledExample = styled.article`
+  background: ${p => p.theme.colour.example};
+  color: white;
+  padding: ${p => 4 * p.theme.spacing}px 0 0;
+`
+
+const Title = styled.h2`
+  font-size: 20px;
+  font-weight: normal;
+  margin: 0 0 ${p => 3 * p.theme.spacing}px;
+`
+
+const Snippet = styled.aside`
+  align-items: stretch;
+  display: flex;
+  flex-flow: row wrap;
+  margin: ${p => 4 * p.theme.spacing}px 0 0;
+
+  pre {
+    border: 1px solid #18191E;
+    border-width: ${p => p.theme.spacing / 4}px 1px ${p => p.theme.spacing / 4}px;
+    flex-grow: 1;
+    margin: 0;
+    text-align: left;
+    width: 30%;
+
+    code {
+      font-family: 'Source Code Pro', monospace;
+    }
+  }
+`
 
 class Example extends PureComponent {
   static propTypes = {
@@ -24,21 +55,21 @@ class Example extends PureComponent {
       ...props
     } = this.props
 
-    return <article {...props}>
-      <h2>{ title }</h2>
+    return <StyledExample {...props}>
+      <Title>{ title }</Title>
 
       { children }
 
       { snippets.length > 0 &&
-        <aside>
+        <Snippet>
           {snippets.map((snippet, index) =>
             <SyntaxHighlighter language='javascript' style={hljsStyle} key={index}>
               { snippet }
             </SyntaxHighlighter>
           )}
-        </aside>
+        </Snippet>
       }
-    </article>
+    </StyledExample>
   }
 }
 
--- example/src/theme.js
+++ example/src/theme.js
@@ -2,6 +2,7 @@ export default {
   colour: {
     background: '#D7DCE5',
     disabled: '#A0A8B7',
+    example: '#21252B',
     primary: '#C45140'
   },
   spacing: 8
