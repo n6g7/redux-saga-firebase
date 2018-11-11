265b57bcd63bc543b0b70e34adf543ff7fd7e4dc
Nathan Gaberel <nathan@gnab.fr>
2018-11-11 23:05:38 +0000

    Reformat definitions.

--- index.d.ts
+++ index.d.ts
@@ -10,7 +10,7 @@ interface SyncOptions<S, T> {
 
 declare enum FirestoreType {
   Collection = 'collection',
-  Document = 'document'
+  Document = 'document',
 }
 
 declare namespace PathOrRef {
@@ -47,47 +47,78 @@ declare namespace ChannelOutput {
 }
 
 interface Auth {
-  applyActionCode: (code: string) => void,
-  channel: () => Channel<ChannelOutput.Auth>,
-  confirmPasswordReset: (code: string, newPassword: string) => void,
-  createUserWithEmailAndPassword: (email: string, password: string) => firebase.User,
-  deleteProfile: () => void,
-  linkWithPopup: (authProvider: firebase.auth.AuthProvider) => firebase.auth.UserCredential,
-  linkWithRedirect: (authProvider: firebase.auth.AuthProvider) => void,
-  sendEmailVerification: (actionCodeSettings: firebase.auth.ActionCodeSettings) => void,
-  sendPasswordResetEmail: (email: string, actionCodeSettings: firebase.auth.ActionCodeSettings) => void,
-  signInAndRetrieveDataWithCredential: (credential: firebase.auth.AuthCredential) => firebase.auth.UserCredential,
-  signInAnonymously: () => firebase.User,
-  signInWithCredential: (credential: firebase.auth.AuthCredential) => firebase.User,
-  signInWithCustomToken: (token: string) => firebase.User,
-  signInWithEmailAndPassword: (email: string, password: string) => firebase.User,
-  signInWithPhoneNumber: (phoneNumber: string, applicationVerifier: firebase.auth.ApplicationVerifier) => firebase.auth.ConfirmationResult,
-  signInWithPopup: (authProvider: firebase.auth.AuthProvider) => firebase.auth.AuthCredential,
-  signInWithRedirect: (authProvider: firebase.auth.AuthProvider) => void,
-  signOut: () => void,
-  unlink: (provider: string) => firebase.User,
+  applyActionCode: (code: string) => void
+  channel: () => Channel<ChannelOutput.Auth>
+  confirmPasswordReset: (code: string, newPassword: string) => void
+  createUserWithEmailAndPassword: (email: string, password: string) => firebase.User
+  deleteProfile: () => void
+  linkWithPopup: (
+    authProvider: firebase.auth.AuthProvider,
+  ) => firebase.auth.UserCredential
+  linkWithRedirect: (authProvider: firebase.auth.AuthProvider) => void
+  sendEmailVerification: (actionCodeSettings: firebase.auth.ActionCodeSettings) => void
+  sendPasswordResetEmail: (
+    email: string,
+    actionCodeSettings: firebase.auth.ActionCodeSettings,
+  ) => void
+  signInAndRetrieveDataWithCredential: (
+    credential: firebase.auth.AuthCredential,
+  ) => firebase.auth.UserCredential
+  signInAnonymously: () => firebase.User
+  signInWithCredential: (credential: firebase.auth.AuthCredential) => firebase.User
+  signInWithCustomToken: (token: string) => firebase.User
+  signInWithEmailAndPassword: (email: string, password: string) => firebase.User
+  signInWithPhoneNumber: (
+    phoneNumber: string,
+    applicationVerifier: firebase.auth.ApplicationVerifier,
+  ) => firebase.auth.ConfirmationResult
+  signInWithPopup: (
+    authProvider: firebase.auth.AuthProvider,
+  ) => firebase.auth.AuthCredential
+  signInWithRedirect: (authProvider: firebase.auth.AuthProvider) => void
+  signOut: () => void
+  unlink: (provider: string) => firebase.User
   updatePassword: (password: string) => void
 }
 
 interface Database {
-  read: (pathOrRef: PathOrRef.Database) => any,
-  create: (pathOrRef: PathOrRef.Database, data: any) => string,
-  update: (pathOrRef: PathOrRef.Database, data: any) => void,
-  patch: (pathOrRef: PathOrRef.Database, data: any) => void,
-  delete: (pathOrRef: PathOrRef.Database) => void,
-  channel: (pathOrRef: PathOrRef.Database, event: string) => Channel<ChannelOutput.Database>,
+  read: (pathOrRef: PathOrRef.Database) => any
+  create: (pathOrRef: PathOrRef.Database, data: any) => string
+  update: (pathOrRef: PathOrRef.Database, data: any) => void
+  patch: (pathOrRef: PathOrRef.Database, data: any) => void
+  delete: (pathOrRef: PathOrRef.Database) => void
+  channel: (
+    pathOrRef: PathOrRef.Database,
+    event: string,
+  ) => Channel<ChannelOutput.Database>
   sync: (pathOrRef: PathOrRef.Database, options: object, event: string) => void
 }
 
 interface Firestore {
-  addDocument: (collectionRef: PathOrRef.Collection, data: object) => firebase.firestore.DocumentReference,
-  channel: (pathOrRef: PathOrRef.Firestore, type: FirestoreType) => Channel<ChannelOutput.Firestore>,
-  deleteDocument: (documentRef: PathOrRef.Document) => void,
-  getCollection: (collectionRef: PathOrRef.Collection) => firebase.firestore.QuerySnapshot,
-  getDocument: (documentRef: PathOrRef.Document) => firebase.firestore.DocumentSnapshot,
-  setDocument: (documentRef: PathOrRef.Document, data: object, options: firebase.firestore.SetOptions) => void,
-  syncCollection: <T>(collectionRef: PathOrRef.Collection, options: SyncOptions<ChannelOutput.Firestore, T>) => void,
-  syncDocument: <T>(documentRef: PathOrRef.Document, options: SyncOptions<ChannelOutput.Firestore, T>) => void,
+  addDocument: (
+    collectionRef: PathOrRef.Collection,
+    data: object,
+  ) => firebase.firestore.DocumentReference
+  channel: (
+    pathOrRef: PathOrRef.Firestore,
+    type: FirestoreType,
+  ) => Channel<ChannelOutput.Firestore>
+  deleteDocument: (documentRef: PathOrRef.Document) => void
+  getCollection: (collectionRef: PathOrRef.Collection) => firebase.firestore.QuerySnapshot
+  getDocument: (documentRef: PathOrRef.Document) => firebase.firestore.DocumentSnapshot
+  setDocument: (
+    documentRef: PathOrRef.Document,
+    data: object,
+    options: firebase.firestore.SetOptions,
+  ) => void
+  syncCollection: <T>(
+    collectionRef: PathOrRef.Collection,
+    options: SyncOptions<ChannelOutput.Firestore, T>,
+  ) => void
+  syncDocument: <T>(
+    documentRef: PathOrRef.Document,
+    options: SyncOptions<ChannelOutput.Firestore, T>,
+  ) => void
   updateDocument: (documentRef: PathOrRef.Document, ...args: any[]) => void
 }
 
@@ -96,18 +127,30 @@ interface Functions {
 }
 
 interface Messaging {
-  channel: () => Channel<ChannelOutput.Messaging.Message>,
-  syncMessages: <T>(options: SyncOptions<ChannelOutput.Messaging.Message, T>) => void,
-  syncToken: <T>(options: SyncOptions<ChannelOutput.Messaging.Token, T>) => void,
+  channel: () => Channel<ChannelOutput.Messaging.Message>
+  syncMessages: <T>(options: SyncOptions<ChannelOutput.Messaging.Message, T>) => void
+  syncToken: <T>(options: SyncOptions<ChannelOutput.Messaging.Token, T>) => void
   tokenRefreshChannel: () => Channel<ChannelOutput.Messaging.Token>
 }
 
 interface Storage {
-  uploadFile: (pathOrRef: PathOrRef.Storage, file, metadata) => firebase.storage.UploadTask,
-  uploadString: (pathOrRef: PathOrRef.Storage, string: string, format: firebase.storage.StringFormat, metadata: firebase.storage.UploadMetadata) => firebase.storage.UploadTask,
-  getDownloadURL: (pathOrRef: PathOrRef.Storage) => string,
-  getFileMetadata: (pathOrRef: PathOrRef.Storage) => firebase.storage.FullMetadata,
-  updateFileMetadata: (pathOrRef: PathOrRef.Storage, newMetadata: firebase.storage.SettableMetadata) => firebase.storage.FullMetadata,
+  uploadFile: (
+    pathOrRef: PathOrRef.Storage,
+    file,
+    metadata,
+  ) => firebase.storage.UploadTask
+  uploadString: (
+    pathOrRef: PathOrRef.Storage,
+    string: string,
+    format: firebase.storage.StringFormat,
+    metadata: firebase.storage.UploadMetadata,
+  ) => firebase.storage.UploadTask
+  getDownloadURL: (pathOrRef: PathOrRef.Storage) => string
+  getFileMetadata: (pathOrRef: PathOrRef.Storage) => firebase.storage.FullMetadata
+  updateFileMetadata: (
+    pathOrRef: PathOrRef.Storage,
+    newMetadata: firebase.storage.SettableMetadata,
+  ) => firebase.storage.FullMetadata
   deleteFile: (pathOrRef: PathOrRef.Storage) => void
 }
 
@@ -124,7 +167,7 @@ declare class ReduxSagaFirebase {
   messaging: Messaging
   storage: Storage
 
-  constructor (firebaseApp: firebase.app.App)
+  constructor(firebaseApp: firebase.app.App)
 
   projectId(): string
 }
